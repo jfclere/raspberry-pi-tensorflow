92861ef1b98a98be8ac6b59f5db3f0b2a7be2aa7
Jean-Frederic Clere <jfclere@gmail.com>
2023-01-07 13:09:27 +0000

    Remove an extra conversion and use a previous downloaded model.

--- camdetect.py
+++ camdetect.py
@@ -6,8 +6,8 @@ import pandas as pd
 from picamera2 import Picamera2
 
 # Carregar modelos
-# detector = hub.load("efficientdet_lite2_detection_1")
-detector = hub.load("https://tfhub.dev/tensorflow/efficientdet/lite2/detection/1")
+detector = hub.load("efficientdet_lite2_detection_1")
+# detector = hub.load("https://tfhub.dev/tensorflow/efficientdet/lite2/detection/1")
 labels = pd.read_csv('labels.csv',sep=';',index_col='ID')
 labels = labels['OBJECT (2017 REL.)']
 
@@ -37,10 +37,10 @@ while(True):
     inp = cv2.resize(frame, (width , height ))
 
     #Convert img to RGB
-    rgb = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
+    #rgb = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
 
     #Is optional but i recommend (float convertion and convert img to tensor image)
-    rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
+    rgb_tensor = tf.convert_to_tensor(inp, dtype=tf.uint8)
 
     #Add dims to rgb_tensor
     rgb_tensor = tf.expand_dims(rgb_tensor , 0)
@@ -59,7 +59,7 @@ while(True):
             continue
             
         score_txt = f'{100 * round(score,0)}'
-        img_boxes = cv2.rectangle(rgb,(xmin, ymax),(xmax, ymin),(0,255,0),1)      
+        img_boxes = cv2.rectangle(inp,(xmin, ymax),(xmax, ymin),(0,255,0),1)      
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(img_boxes,label,(xmin, ymax-10), font, 0.5, (255,0,0), 1, cv2.LINE_AA)
         cv2.putText(img_boxes,score_txt,(xmax, ymax-10), font, 0.5, (255,0,0), 1, cv2.LINE_AA)
@@ -71,7 +71,7 @@ while(True):
     except NameError: img_boxes = None
     if img_boxes is None:
         print("Nothing found!!!")
-        cv2.imwrite("/tmp/now.jpg", rgb)
+        cv2.imwrite("/tmp/now.jpg", inp)
     else:
         cv2.imwrite("/tmp/now.jpg", img_boxes)
         break
